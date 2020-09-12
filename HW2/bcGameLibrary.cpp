4e9e4fbafe3016ef1f3a190029021c16eb8fc28f
MalachiPayne <malachimpayne@gmail.com>
2020-09-11 21:42:20 -0700

    Started working on HW2

--- HW2/bcGameLibrary.cpp
+++ HW2/bcGameLibrary.cpp
@@ -1,14 +1,14 @@
 /* 
-    Program Name: HelloWorld
+    Program Name: bcGameLibrary
     Author: Malachi Payne
     Course: COMP B12
     Date: 9.11.2020
 */
 #include <iostream>
+#include <cmath>
+#include <iomanip>
 using namespace std;
 
 int main()
 {
-    cout << "hello world!" << endl;
-    return 0;
 }
