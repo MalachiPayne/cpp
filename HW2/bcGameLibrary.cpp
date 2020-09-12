c616dce9c9d462953161a5bf1bd9ead90fabe233
MalachiPayne <malachimpayne@gmail.com>
2020-09-11 22:24:45 -0700

    edited the bcGameLibrary.cpp

--- HW2/bcGameLibrary.cpp
+++ HW2/bcGameLibrary.cpp
@@ -1,5 +1,5 @@
 /* 
-    Program Name: bcGameLibrary
+    Program Name: BC Game Library (bcGameLibrary.cpp)
     Author: Malachi Payne
     Course: COMP B12
     Date: 9.11.2020
@@ -7,8 +7,10 @@
 #include <iostream>
 #include <cmath>
 #include <iomanip>
+#include <ctime>
 using namespace std;
 
 int main()
 {
+
 }
